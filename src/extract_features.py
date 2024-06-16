8256e6e06baa3f30ad7a9e053139e453ba8f3f66
przybytniowskaj <01161611@pw.edu.pl>
2024-06-16 09:56:53 +0200

    final restructurization and results

--- src/extract_features.py
+++ src/extract_features.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 
 def extract_features(full_path):
@@ -10,7 +11,9 @@ def extract_features(full_path):
     features["Dataset"] = parts[1].split("-")[0]
     features["Epochs"] = parts[1].split("-")[1].split("_")[1]
     features["Attention Type"] = parts[2]
-    features["Optimizer"] = "AdamW" if "adamw" in parts[3] else "Adam"
+    features["Optimizer"] = (
+        "AdamW" if "adamw" in parts[3] else ("Adam" if "adam" in parts[3] else "SGD")
+    )
     features["Window Size"] = int(parts[4].split("_")[-1])
     return features
 
@@ -23,7 +26,14 @@ def load_json(file):
 
 def collect_results(folder_path):
     data = []
-    hyperparameters = [(0.25, 0.75), (0.5, 0.5), (0.75, 0.25)]
+    hyperparameters = [
+        # (0.1, 0.9),
+        # (0.15, 0.85),
+        # (0.2, 0.8),
+        (0.25, 0.75),
+        (0.5, 0.5),
+        (0.75, 0.25),
+    ]
     for root, dirs, _ in os.walk(folder_path):
         features = {}
         for dir_name in dirs:
@@ -47,14 +57,30 @@ def collect_results(folder_path):
                 features["path_to_val_results"] = os.path.join(
                     full_path, "val_losses.csv"
                 )
-                for alpha, betha in hyperparameters:
-                    cost_function = alpha * features["GPU Memory Usage"] + betha * (
-                        1 - features["Test Accuracy"]
-                    )
-                    features[f"Cost Function ({alpha}, {betha})"] = cost_function
                 data.append(features)
     data = pd.DataFrame(data)
-    data.to_csv("extracted_features.csv", index=False)
+
+    datasets = data["Dataset"].unique()
+    data_scaled = []
+    for dataset in datasets:
+        dataset_data = data[data["Dataset"] == dataset]
+        dataset_data["GPU Memory Usage"] = MinMaxScaler().fit_transform(
+            dataset_data["GPU Memory Usage"].values.reshape(-1, 1)
+        )
+        dataset_data["Test Accuracy"] = MinMaxScaler().fit_transform(
+            dataset_data["Test Accuracy"].values.reshape(-1, 1)
+        )
+        dataset_data["Test Error"] = 1 - dataset_data["Test Accuracy"]
+        data_scaled.append(dataset_data)
+    data_scaled = pd.concat(data_scaled, axis=0)
+
+    for alpha, betha in hyperparameters:
+        cost_function = alpha * data_scaled["GPU Memory Usage"] + betha * (
+            data_scaled["Test Error"]
+        )
+        data_scaled[f"Cost Function ({alpha}, {betha})"] = cost_function
+    # data_scaled = data_scaled.reset_index(drop=True)
+    data_scaled.to_csv("extracted_features.csv", index=False)
     print('Data saved into "extracted_features.csv".')
 
 
